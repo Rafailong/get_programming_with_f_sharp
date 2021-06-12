59836d1c39a079c197f612c70608e9883262561c
Rafa Avila <rafael.avila@mastercard.com>
2021-06-12 13:56:59 -0500

    Unit 1 capstone. A more type-safe solution.

--- src/Unit1/Program.fs
+++ src/Unit1/Program.fs
@@ -1,5 +1,4 @@
 module Unit1
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
 
@@ -8,6 +7,19 @@ type Destination =
     | Office
     | Stadium
     | Gas_Station
+
+type Result =
+    | InvalidDestiny of invalid: string
+    | NotEnoughPetrol of petrol: int * destiniy: Destination
+    | StayingAtLocation of petrol: int * location: Destination
+    | Travel of petrol: int * origin: Destination * destiny: Destination
+
+    member this.PrettyPrint =
+        match this with
+        | InvalidDestiny (invalid) -> sprintf "Invalid destiny = %s" invalid
+        | NotEnoughPetrol (petrol, destiny) -> sprintf "Not enough petrol (%d) to travel to %A" petrol destiny
+        | StayingAtLocation (petrol, location) -> sprintf "staying at same location %A with %d petrol units" location petrol
+        | Travel (petrol, origin, destiny) -> sprintf "Travel from %A to %A with %d petrol units" origin destiny petrol
 
 let requiredPetroUnits d =
     match d with
@@ -26,35 +38,47 @@ let parseDestiny str =
 
 let travel petrol origin destiny =
     match (origin, destiny) with
-    | (Gas_Station, Gas_Station) -> Some(petrol + 50)
-    | (o, d) when o = d -> Some petrol
+    | (Gas_Station, Gas_Station) -> StayingAtLocation (petrol+50, origin)
+    | (o, d) when o = d -> StayingAtLocation (petrol, origin)
     | (_, d) ->
         let requiredPetrol = requiredPetroUnits d
 
-        if petrol < requiredPetrol then
-            None
-        else
-            printfn "Traveling to %A" destiny
-            Some(petrol - requiredPetrol)
+        if petrol < requiredPetrol then NotEnoughPetrol (petrol, destiny)
+        else Travel (petrol-requiredPetrol, origin, destiny)
 
-let rec program petrol origin =
-    printfn "Stats: Petrol: %d - Location: %A" petrol origin
-    printfn "Enter you destination: home, office, stadium, gas"
-
-    let input = Console.ReadLine()
+let rec program inputReader logEvent petrol origin =
+    let input: string = inputReader()
     let maybeDestiny = parseDestiny (input.Trim())
+
+    let partial = program inputReader logEvent
 
     match maybeDestiny with
     | Some destiny ->
-        let somePetrol = travel petrol origin destiny
-
-        match somePetrol with
-        | Some left -> program left destiny
-        | None -> printfn "Not enough petrol to travel."
-    | None -> printfn "Going nowhere. Invalid destination."
+        let result = travel petrol origin destiny
+        logEvent result
+        match result with
+        | Travel (petrol, _, destiny) -> partial petrol destiny
+        | NotEnoughPetrol (petrol, _) -> partial petrol origin
+        | StayingAtLocation (petrol, location) -> partial petrol location
+        | invalidDestiny -> invalidDestiny
+    | None ->
+        let result = InvalidDestiny input
+        logEvent result
+        result
 
 [<EntryPoint>]
 let main argv =
-    program 100 Home
+
+    let inputReader = fun () ->
+        printfn "Enter you destination: home, office, stadium, gas"
+        let input = Console.ReadLine()
+        input.Trim()
+
+    let logEvent (result: Result) =
+        printfn "%s" result.PrettyPrint
+
+    program inputReader logEvent 100 Home |> ignore
+    //dumpHistory() |> ignore
+
     printfn "Program terminated."
     0
